25894b0df9868c4cef122328501a962ca8d60057
14A_formation D14A 755 <14A_formation.D14A@si.cnaf.info>
2019-11-04 15:25:05 +0100

    test commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ngModuleJitUrl } from '@angular/compiler';
+import { UiModule } from './ui/ui.module';
+import { LoginModule } from './login/login.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    UiModule,
+    LoginModule
   ],
   providers: [],
   bootstrap: [AppComponent]
